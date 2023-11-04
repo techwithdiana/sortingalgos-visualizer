660a242a4cf69a1fe2d9c7d755ca3af3e3fdb94d
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2023-11-04 03:29:12 -0400

    added array bar css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import HeaderBar from './components/HeaderBar';
 import PopUpInfo from './components/PopUpInfo'; 
 import './App.css'; 
+import ArrayBar from './components/ArrayBar';
 function App() {
   return (
-    <div className="App">
-      <HeaderBar />
-      <PopUpInfo />
+    <div className='main-container'>
+      <div className="App">
+        <HeaderBar />
+        <PopUpInfo />
+        <ArrayBar array={[10, 40, 20, 30, 50]}/>
+      </div>
     </div>
   );
 }
